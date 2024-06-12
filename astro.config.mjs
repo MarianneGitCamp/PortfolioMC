29c82ea9cb7f9e3a7ce632b63d96ac1be0e8c8ff
Marianne C <Mariannecampguilhem@gmail.com>
2024-06-12 18:20:20 +0200

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+
+export default defineConfig({
+    site: 'https://marianoukarianne.github.io',
+    base : '/PortfolioMC' , 
+});
