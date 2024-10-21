c16316e203a00a005ab35481dbb6ef6d61bb3afd
Marianne C <Mariannecampguilhem@gmail.com>
2024-10-21 12:50:11 +0200

    change of site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 
 export default defineConfig({
-    site: "https://MARIANouKARIANNE/github.io",
+    site: "https://MarianneGitCamp/github.io",
     base : "/PortfolioMC"
 });
